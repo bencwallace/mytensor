8849077704bc38659ba8add745ee33c61d7de5c4
Ben Wallace <bencwallace@gmail.com>
2021-07-18 13:32:16 -0400

    created environment

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup, Extension
+
+
+def main():
+	setup(
+		name="fputs",
+		version="1.0.0",
+		description="Python interface for the fputs C library function",
+		ext_modules=[Extension("mytensor", ["mytensor.c"])],
+	)
+
+
+if __name__ == "__main__":
+	main()
